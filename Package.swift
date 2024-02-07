260796a52d7d32bd4994ccd54cababc1cf302f8f
Vincent Coetzee <vincent.coetzee@icloud.com>
2024-02-07 13:35:53 +0200

    Initial commit

--- Package.swift
+++ Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "Modeling",
+    platforms: [
+           .iOS(.v16),
+           .macOS(.v10_15) // Limit the deployment of this package to iOS 16 and above, this is needed because SystemLogger makes use of the Apple SOLog framework containing the Logger implementation
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Modeling",
             targets: ["Modeling"]),
     ],
+    dependencies: [
+        .package(path: "../Logging")
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Modeling"),
+            name: "Modeling",
+            dependencies: [
+                .product(name: "Logging",package: "Logging")
+                ]),
         .testTarget(
             name: "ModelingTests",
             dependencies: ["Modeling"]),
